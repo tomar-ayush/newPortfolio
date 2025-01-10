a701ac37b754e6c8088067719d71b70f0969e7ba
Aryainguz <aryaninguz369@gmail.com>
2025-01-10 12:00:00 +0530

    feat: added hero component

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["geist"],
+  images: {
+    domains: ["i.imgur.com", "github.com"],
+  },
+};
+
+module.exports = nextConfig;
